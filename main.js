7f06cda18329fb41f69b8d38b9e9b6fe5f38ed1a
Madhvi Saxena <ssms122005@gmail.com>
2023-12-25 18:33:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -33,3 +33,40 @@ const deletePassword = (website)=>{
     showPasswords()
 
 }
+
+// Logic to fill the table
+const showPasswords = () => {
+    let tb = document.querySelector("table")
+    let data = localStorage.getItem("passwords")
+    if (data == null || JSON.parse(data).length == 0) {
+        tb.innerHTML = "No Data To Show"
+    }
+    else {
+        tb.innerHTML =  `<tr>
+        <th>Website</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Delete</th>
+    </tr> `
+        let arr = JSON.parse(data);
+        let str = ""
+        for (let index = 0; index < arr.length; index++) {
+            const element = arr[index];
+
+            str += `<tr>
+    <td>${element.website} <img onclick="copyText('${element.website}')" src="./copy.svg" alt="Copy Button" width="10" width="10" height="10">
+    </td>
+    <td>${element.username} <img onclick="copyText('${element.username}')" src="./copy.svg" alt="Copy Button" width="10" width="10" height="10">
+    </td>
+    <td>${maskPassword(element.password)} <img onclick="copyText('${element.password}')" src="./copy.svg" alt="Copy Button" width="10" width="10" height="10">
+    </td>
+    <td><button class="btnsm" onclick="deletePassword('${element.website}')">Delete</button></td>
+        </tr>`
+        }
+        tb.innerHTML = tb.innerHTML + str
+
+    }
+    website.value = ""
+    username.value = ""
+    password.value = ""
+}
